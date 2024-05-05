ef8cb49323f3f6255206d7d311cc417ddde8e0b0
Santiago Guastavino <san.guasta@gmail.com>
2024-05-05 17:57:23 -0300

    feat: improved the logger middleware

--- src/Infrastructure/middleware/logger.middleware.ts
+++ src/Infrastructure/middleware/logger.middleware.ts
@@ -6,19 +6,21 @@ export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { method, originalUrl } = request;
+    const { method, originalUrl, body } = request;
 
     response.on('finish', () => {
-      const { statusCode } = response;
+      const { statusCode, statusMessage } = response;
+
+      let log: string = `Request: ${method} ${originalUrl} - Response: ${statusMessage} ${statusCode}`;
+
+      if (body && Object.keys(body).length > 0) {
+        log = log + ` - Request body ${JSON.stringify(body)}`;
+      }
 
       if (statusCode >= 400) {
-        this.logger.error(
-          `Request: ${method} ${originalUrl} - Response status: ${statusCode}`,
-        );
+        this.logger.error(log);
       } else {
-        this.logger.log(
-          `Request: ${method} ${originalUrl} - Response status: ${statusCode}`,
-        );
+        this.logger.log(log);
       }
     });
 
